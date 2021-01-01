652045431ebeb2213c3d59c131fa65b3f9bbaae6
diegospm <diego-spm@hotmail.com>
2021-01-01 19:52:35 -0300

    adding json response implementation

---
+++ src/HttpProvider/Responses/JsonResponse.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpProvider\Responses;
+
+use HttpProvider\Interfaces\ResponseInterface;
+
+class JsonResponse implements ResponseInterface
+{
+    private string $content;
+
+    private int $code;
+
+    public function setContent(string $content): ResponseInterface
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setStatusCode(int $code): ResponseInterface
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->code;
+    }
+
+    public function toArray(): ?array
+    {
+        return json_decode($this->content, true);
+    }
+
+    public function toObject(): ?object
+    {
+        return json_decode($this->content);
+    }
+}
